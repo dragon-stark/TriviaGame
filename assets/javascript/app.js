e9ad1f8153c72390f5b379ce960991a5bef61f32
DStark <hindash@email.arizona.edu>
2019-11-23 16:46:37 -0700

    better functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,99 +1,178 @@
-//  Trivia Game Timer
+//  Trivia Game
+
+$(document).ready(function ()
+{
+
+  // event listeners
+  $("#remaining-time").hide();
+  $("#start").on('click', trivia.startGame);
+  $(document).on('click', '.option', trivia.guessChecker);
+
+})
+
+var trivia = {
+
+  correct: 0,
+  incorrect: 0,
+  unanswered: 0,
+  currentSet: 0,
+  timer: 90,
+  timerOn: false,
+  timerId: '',
 
 
+  // questions, options and answers
+
+  questions: {
+    q1: "How many colors are there in a rainbow?",
+    q2: "What is the only rock regularly eaten by humans?",
+    q3: "What is the Worlds largest ocean?"
+  },
+
+  options: {
+    q1: ["Six", "Five", "Seven", "Ten"],
+    q2: ["Chocolate rocks", "Salt", "Fudge rocks", "Punk rock"],
+    q3: ["Atlantic", "Arctic", "Pacific", "Indian"]
+  },
+
+  answers: {
+    q1: 'Seven',
+    q2: 'Salt',
+    q3: 'Pacific'
+  },
+  //Start game
+  startGame: function ()
+  {
+    // restarting game results
+    trivia.currentSet = 0;
+    trivia.correct = 0;
+    trivia.incorrect = 0;
+    trivia.unanswered = 0;
+    clearInterval(trivia.timerId);
+
+    // show game section
+    $('#game').show();
+
+    //  empty last results
+    $('#results').html('');
+
+    $('#timer').text(trivia.timer);
+
+    // remove start button
+    $('#start').hide();
+
+    $('#remaining-time').show();
+
+    // ask question
+    trivia.nextQuestion();
+
+  },
+  //  display questions and options
+  nextQuestion: function ()
+  {
+
+    // set timer per question
+    trivia.timer = 30;
+    $('#timer').removeClass('last-seconds');
+    $('#timer').text(trivia.timer);
 
 
+    if (!trivia.timerOn) {
+      trivia.timerId = setInterval(trivia.timerRunning, 1000);
+    }
 
+    // gets all the questions
+    var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+    $('#question').text(questionContent);
 
+    //  options for the question
+    var questionOptions = Object.values(trivia.options)[trivia.currentSet];
 
+    // options to  html
+    $.each(questionOptions, function (index, key)
+    {
+      $('#options').append($('<button class="option btn btn-info btn-lg">' + key + '</button>'));
+    })
 
-// $("#target").click(function ()) {
-//   alert("You have two minutes to answer three questions");
-// };
-// setTimeout(gamelost, 120000);
+  },
+  // Timer Countdown
+  timerRunning: function ()
+  {
+    // if timer still has time left and there are still questions left to ask
+    if (trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length) {
+      $('#timer').text(trivia.timer);
+      trivia.timer--;
+      if (trivia.timer === 4) {
+        $('#timer').addClass('last-seconds');
+      }
+    }
+    // out of time
+    else if (trivia.timer === -1) {
+      trivia.unanswered++;
+      trivia.result = false;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Sorry you are Out of time! The answer is ' + " " + Object.values(trivia.answers)[trivia.currentSet] + '</h3>');
+    }
+    // if all the questions have  shown end the game, show results
+    else if (trivia.currentSet === Object.keys(trivia.questions).length) {
 
+      // adds results of game
+      $('#results')
+        .html('<h3>Thanks for playing!</h3>' +
+          '<p>Correct: ' + trivia.correct + '</p>' +
+          '<p>Incorrect: ' + trivia.incorrect + '</p>' +
+          '<p>Unanswered: ' + trivia.unanswered + '</p>' +
+          '<p>Click to play again!</p>');
 
-// button.onClick = function () {
+      // hide game section
+      $('#game').hide();
 
-//   alert("Are you ready to play?");
+      // show start button to begin a new game
+      $('#start').show();
+    }
 
-// };
+  },
 
+  guessChecker: function ()
+  {
 
-// //  Step 2:
-// //  after 5 seconds, execute the fiveSeconds function
-// //  after 10 seconds, execute the tenSeconds function
-// //  after 15 seconds, execute the timeUp function
+    // gameResult setTimeout
+    var resultId;
 
-// setTimeout(fiveSeconds, 1000 * 10);
-// setTimeout(tenSeconds, 1000 * 20);
-// setTimeout(timeUp, 1000 * 30);
+    // the answer to the  question
+    var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
 
-// //  Step 3:
-// //  Fill in the blanks to these functions.
-// function fiveSeconds () {
-//   // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
-//   // console log 10 seconds left
-//   $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
-//   console.log("10 seconds left");
-// }
+    // if answer is right, add correct
+    if ($(this).text() === currentAnswer) {
 
-// function tenSeconds () {
-//   // in the element with an id of time-left add an h2 saying About 5 Seconds Left!
-//   // console log 5 seconds left
-//   $("#time-left").append("<h2>About 5 Seconds Left!</h2>");
-//   console.log("5 seconds left");
-// }
+      trivia.correct++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Great Correct Answer!</h3>');
+    }
+    // wrong answers
+    else {
 
+      trivia.incorrect++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>No, Better luck next time! ' + currentAnswer + '</h3>');
+    }
 
+  },
+  //remove previous question
+  guessResult: function ()
+  {
+    // next question
+    trivia.currentSet++;
 
-// setTimeout(gamelost, 120000);
+    // remove the options and results
+    $('.option').remove();
+    $('#results h3').remove();
 
+    trivia.nextQuestion();
 
-//  better attempt at this 
+  }
 
-//  Variable that will hold the button alert's timeout when it is clicked.
-var ButtonAlert;
-
-//  
-//  This alert will run one second after the page has loaded.
-var windowTimeout = setTimeout(function () {
-  alert("Hi There! Click Start Game to beging your Trivia Game!");
-}, 1000);
-
-//  Start on click.
-$("#start").on("click", function () {
-  //  This button to run timer for three seconds after the function is called.
-  delayButtonAlert = setTimeout(function () {
-    alert("Alert #2: Called 3 seconds after the start button is clicked.");
-  }, 3000);
-});
-
-//  Start on click.
-$("#start").on("click", function () {
-  //  Set the button alert's timeout to run three seconds after the function's called.
-  delayButtonAlert = setTimeout(function () {
-    alert("Alert #2: Called 3 seconds after the start button is clicked.");
-  }, 3000);
-});
-
-//  Start on click.
-$("#start").on("click", function () {
-  //  Set the button alert's timeout to run three seconds after the function's called.
-  delayButtonAlert = setTimeout(function () {
-    alert("Alert #2: Called 3 seconds after the start button is clicked.");
-  }, 3000);
-});
-function timeUp () {
-  // in the element with an id of time-left add an h2 saying Time's Up!
-
-  console.log("done");
-  $("#time-left").append("<h2>Time's Up!</h2>");
-  console.log("time is up");
-  y();
 }
-//  Cancel window alert on click.
-$("#window-cancel").on("click", function () {
-  //  Clear the timeout, and stop the window alert.
-  clearTimeout(windowTimeout);
-});
